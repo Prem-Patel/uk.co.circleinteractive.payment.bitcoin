6f3a1c1f7a4a29b5dee37e63d03ceb2e9b9f6781
andy-walker <andyw+github2@circle-interactive.co.uk>
2014-07-29 23:33:07 +0100

    bugfix

--- custom/php/BitPay/Payment/IPN.php
+++ custom/php/BitPay/Payment/IPN.php
@@ -32,7 +32,7 @@ class BitPay_Payment_IPN extends CRM_Core_Payment_BaseIPN {
             # write updated status back to database
             BitPay_Payment_BAO_Transaction::save($response + array(
                 'contribution_id' => $invoice['contribution_id'],
-                'bitpay_id'       => $bitpay_id
+                'bitpay_id'       => $invoice['id']
             ));
 
             # if anything other than complete, return for now
